3d7e718ac46488093a0178a19ee6e9600f95d6c3
Manny <mtole035@fiu.edu>
2023-02-16 21:40:30 -0500

    Update init.lua

--- nvChad/nvim/lua/custom/init.lua
+++ nvChad/nvim/lua/custom/init.lua
@@ -9,10 +9,11 @@ local autocmd = vim.api.nvim_create_autocmd
 
 if g.neovide then
   g.neovide_transparency = 0.8
-  g.neovide_refresh_rate = 144
-  g.neovide_cursor_vfx_mode = 'ripple'
+  g.neovide_refresh_rate = 120
+  g.neovide_cursor_vfx_mode = "railgun"
   g.neovide_cursor_animation_length = 0.03
   g.neovide_cursor_trail_size = 0.9
   g.neovide_remember_window_size = true
   g.neovide_padding_top = 50
+  vim.opt.guifont = { "iosevka", ":h20" }
 end
